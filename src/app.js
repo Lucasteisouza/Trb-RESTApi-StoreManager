e12a0bf144f42f756773da8d563209e83b76742a
Lucasteisouza <lucasteisouza@gmail.com>
2023-03-21 15:18:40 -0300

    Commit inicial

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
-});
+}); 
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
