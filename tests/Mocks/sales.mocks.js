5c3f43a73808a116972d037b7270a4b9ab8ec680
Lucasteisouza <lucasteisouza@gmail.com>
2023-03-23 21:59:18 -0300

    fix: ajusta o teste para corresponder a sintaxe correta da resposta da API

--- tests/Mocks/sales.mocks.js
+++ tests/Mocks/sales.mocks.js
@@ -1,6 +1,6 @@
 const newSaleMock = {
   id: 1,
-  itensSold: [{ productId: 1, quantity: 1 }, { productId: 2, quantity: 5}],
+  itemsSold: [{ productId: 1, quantity: 1 }, { productId: 2, quantity: 5}],
 };
 
 const newSaleRequestMock =[{ productId: 1, quantity: 1 }, { productId: 2, quantity: 5}];
--- tests/unit/models/sales.models.test.js
+++ tests/unit/models/sales.models.test.js
@@ -8,8 +8,8 @@ describe('testa a camada model da rota sales', async () => {
   it('testa a função createSale', async () => {
     const stub = sinon.stub(connection, "execute");
     stub.onCall(0).resolves([{ insertId: newSaleMock.id }]);
-    stub.onCall(1).resolves(newSaleMock.itensSold[0]);
-    stub.onCall(2).resolves(newSaleMock.itensSold[1]);
+    stub.onCall(1).resolves(newSaleMock.itemsSold[0]);
+    stub.onCall(2).resolves(newSaleMock.itemsSold[1]);
     const result = await salesModels.createSale(newSaleRequestMock);
     expect(result).to.be.deep.equal(newSaleMock);
     stub.restore();
